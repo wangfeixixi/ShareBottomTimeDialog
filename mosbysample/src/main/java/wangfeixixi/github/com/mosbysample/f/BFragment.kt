3d647359a5753defc6d361f10a9484c3bc5c3303
xuanyuanxixi <1020301980@qq.com>
2018-05-14 18:12:55 +0800

    kotlin测试viewstub

--- mosbysample/src/main/java/wangfeixixi/github/com/mosbysample/f/BFragment.kt
+++ mosbysample/src/main/java/wangfeixixi/github/com/mosbysample/f/BFragment.kt
@@ -1,49 +1,40 @@
 package wangfeixixi.github.com.mosbysample.f
 
-import android.util.Log
 import android.view.View
 import kotlinx.android.synthetic.main.b_fragment.*
-import kotlinx.android.synthetic.main.vs_test.*
 import kotlinx.android.synthetic.main.vs_test.view.*
 import wangfeixixi.github.com.mosbysample.R
 import wangfeixixi.github.com.mosbysample.base.BaseF
 
 class BFragment : BaseF<BView, BPresent>(), BView, View.OnClickListener {
-    override fun onClick(v: View?) {
+    var isShow = false;
 
+    override fun onClick(v: View?) {
+        isShow = !isShow
+        showNoNet(isShow)
     }
 
     private var TAG = "BFragment"
 
+
     override fun initView() {
         tv_name.text = TAG
         tv_name.setOnClickListener(this)
-
-//        vs_test!!.inflate();
-        showNoNet(true)
     }
 
+    var v_no_net: View? = null
+
     fun showNoNet(isShow: Boolean) {
-        if (isShow) {
-            try {
-                val view = vs_test!!.inflate();
-//                view
-                view!!.tv_test_test!!.setOnClickListener(View.OnClickListener {
-                    TAG += "1";
-                    tv_name.text = TAG
-                })
-            } catch (e: Exception) {
-
-            }
-
-        }
-        vs_test.visibility = if (isShow) View.VISIBLE else View.GONE
-//        vs_test.visibility(if(isShow)View.VISIBLE else View.GONE)
+        v_no_net = vs_test?.inflate() ?: v_no_net
+        v_no_net?.tv_test_test?.setOnClickListener(View.OnClickListener {
+            TAG += "1";
+            tv_name.text = TAG
+        })
+        v_no_net?.visibility = if (isShow) View.VISIBLE else View.GONE
     }
 
     override fun initData(firstLoad: Boolean, isVisibleToUser: Boolean) {
-
-        Log.d(TAG, "firstLoad" + firstLoad + "isVisibleToUser" + isVisibleToUser)
+//        Log.d(TAG, "firstLoad" + firstLoad + "isVisibleToUser" + isVisibleToUser)
     }
 
     override fun createPresenter(): BPresent {
@@ -51,7 +42,6 @@ class BFragment : BaseF<BView, BPresent>(), BView, View.OnClickListener {
     }
 
     override fun initContentRes(): Int = R.layout.b_fragment
-
 
 
 }
