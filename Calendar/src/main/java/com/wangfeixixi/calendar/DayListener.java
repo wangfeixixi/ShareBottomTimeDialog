f3e2a71e989725c83e5daf20f0e12d6afcade977
xuanyuanxixi <1020301980@qq.com>
2018-05-17 15:51:15 +0800

    减少类库

--- Calendar/src/main/java/com/wangfeixixi/calendar/DayListener.java
+++
@@ -1,8 +0,0 @@
-package com.wangfeixixi.calendar;
-
-public interface DayListener {
-    void onDayClick(CalendarDay firstDay);
-
-    void onDaysSelected(CalendarDay firstDay, CalendarDay lastDay);
-
-}
--- Calendar/src/main/java/com/wangfeixixi/calendar/SelectedDays.java
+++
@@ -1,9 +0,0 @@
-package com.wangfeixixi.calendar;
-
-import java.io.Serializable;
-//
-//public class SelectedDays implements Serializable {
-//    private static final long serialVersionUID = 3942549765282708376L;
-//    private K first;
-//    private K last;
-//}
