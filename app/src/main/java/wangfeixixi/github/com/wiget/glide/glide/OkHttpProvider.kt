7413fff4dbb2fb4f90028e0aba99e94bc55e71c3
Fei.Wang20 <xuanyuanxixi@foxmail.com>
2018-06-07 13:13:07 +0800

    精简glideokhttp

--- app/src/main/java/wangfeixixi/github/com/wiget/glide/glide/OkHttpProvider.kt
+++ app/src/main/java/wangfeixixi/github/com/wiget/glide/glide/OkHttpProvider.kt
@@ -1,20 +1,20 @@
-package com.xingen.glideokhttp.librarymodule
-
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-
-internal class OkHttpProvider{
-    companion object{
-        /**
-         * 自定义配置OkHttpClient
-         */
-        fun createOkHttpClient(): OkHttpClient {
-            var  builder= OkHttpClient.Builder()
-            var  loggingInterceptor= HttpLoggingInterceptor()
-            loggingInterceptor.level= HttpLoggingInterceptor.Level.BODY
-            builder.addInterceptor(loggingInterceptor)
-
-            return builder.build()
-        }
-    }
-}
+//package com.xingen.glideokhttp.librarymodule
+//
+//import okhttp3.OkHttpClient
+//import okhttp3.logging.HttpLoggingInterceptor
+//
+//internal class OkHttpProvider{
+//    companion object{
+//        /**
+//         * 自定义配置OkHttpClient
+//         */
+//        fun createOkHttpClient(): OkHttpClient {
+//            var  builder= OkHttpClient.Builder()
+//            var  loggingInterceptor= HttpLoggingInterceptor()
+//            loggingInterceptor.level= HttpLoggingInterceptor.Level.BODY
+//            builder.addInterceptor(loggingInterceptor)
+//
+//            return builder.build()
+//        }
+//    }
+//}
