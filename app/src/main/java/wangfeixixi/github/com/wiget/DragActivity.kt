8aa092382717c855fa13f78abf0226fdeec2ffc8
Fei.Wang20 <xuanyuanxixi@foxmail.com>
2018-08-07 09:16:07 +0800

    a

--- app/src/main/java/wangfeixixi/github/com/wiget/DragActivity.kt
+++ app/src/main/java/wangfeixixi/github/com/wiget/DragActivity.kt
@@ -6,9 +6,6 @@ import android.view.View
 import kotlinx.android.synthetic.main.drag_activity.*
 import wangfeixixi.share.drag.DragLayout
 
-/**
- * Created by Fei.Wang20 on 2018/5/30.
- */
 class DragActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +15,7 @@ class DragActivity : AppCompatActivity() {
 
 
     private fun initPoiLayout() {
-//        poi_layout!!.setOnChangeListener(object : DragLayout.OnChangeListener {
-//            override fun onChange(status: Int) {
-//                tv_bottom!!.visibility = if (status == DragLayout.STATUS_CLOSE) View.VISIBLE else View.GONE
-//            }
-//
-//            override fun onScroll(offset: Float) {
-//                tv_bottom!!.visibility = if (offset == 1f) View.VISIBLE else View.GONE
-//            }
-//        })
-//        tv_bottom!!.setOnTikListener { poi_layout!!.toggle(DragLayout.STATUS_EXTEND) }
+
     }
 
 }
